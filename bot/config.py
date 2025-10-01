92e1460a074b015153e54b33a7bcd2239a4fd801
MikaMk4 <mikail.yaldiz@gmx.de>
2025-10-01 14:52:10 +0200

    fixed paths to Archipelago files

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ def load_config():
         "discord_token": os.getenv("DISCORD_TOKEN"),
         "guild_id": int(os.getenv("GUILD_ID")) if os.getenv("GUILD_ID") else None,
         "server_public_ip": os.getenv("SERVER_PUBLIC_IP"),
-        "archipelago_path": "/opt/archipelago/squashfs-root",
+        "archipelago_path": "/opt/archipelago/squashfs-root/opt/Archipelago",
         "archipelago_port": 38281,
         "data_path": "/app/data",
         "whitelist_path": "/app/data/whitelist.json",
